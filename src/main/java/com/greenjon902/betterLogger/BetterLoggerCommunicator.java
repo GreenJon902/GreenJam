021e70f065f23dd755c274cf710fe16659145cb0
GreenJon902 <GreenJon902@gmail.com>
2021-12-26 16:37:01 +0000

    suckseffuly communicate between python and java and sortove log

--- src/main/java/com/greenjon902/betterLogger/BetterLoggerCommunicator.java
+++ src/main/java/com/greenjon902/betterLogger/BetterLoggerCommunicator.java
@@ -4,14 +4,23 @@ import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 public class BetterLoggerCommunicator {
+    private static final int pythonConn_typeLength = 4;
+    private static final int pythonConn_messageLength = 8;
+
     private static final File pythonFile = new File(System.getProperty("user.dir"), "betterLoggerPortal.py");
 
     private boolean started = false;
     private ServerSocket serverSocket;
+    InputStream pythonConnectionInputStream;
+    Thread loggerHandlerThread;
+    Process pythonProcess;
 
     private void openServer() throws IOException {
         String port = System.getenv("BETTERLOGGERPORT");
@@ -32,12 +41,49 @@ public class BetterLoggerCommunicator {
             System.out.println("Copied betterLoggerPortal.py");
         }
 
-        ProcessBuilder processBuilder = new ProcessBuilder("python", pythonFile.getAbsolutePath(), String.valueOf(port));
-        Process process = processBuilder.start();
+        ProcessBuilder processBuilder = new ProcessBuilder("python3", pythonFile.getAbsolutePath(), String.valueOf(port));
+        pythonProcess = processBuilder.start();
 
         System.out.println("Waiting on connection");
         Socket pythonConnection = serverSocket.accept();
-        System.out.println("Incoming connection from " + pythonConnection.getInetAddress().toString() + pythonConnection.getPort());
+        pythonConnectionInputStream = pythonConnection.getInputStream();
+        System.out.println("Incoming connection from " + pythonConnection.getInetAddress().toString() + ":" + pythonConnection.getPort());
+    }
+
+    private void handleLogger() {
+        System.out.println("Handling logger on thread " + Thread.currentThread().getName());
+        while (true) {
+            try {
+                byte[] typeLengthBytes = new byte[pythonConn_typeLength];
+                int bytes_read = pythonConnectionInputStream.read(typeLengthBytes);
+
+                if (bytes_read != -1) {
+                    byte[] typeBytes = new byte[Integer.parseInt(new String(typeLengthBytes, StandardCharsets.UTF_8))];
+                    //noinspection ResultOfMethodCallIgnored
+                    pythonConnectionInputStream.read(typeBytes);
+
+                    String type = new String(typeBytes, StandardCharsets.UTF_8);
+
+
+                    byte[] messageLengthBytes = new byte[pythonConn_messageLength];
+                    //noinspection ResultOfMethodCallIgnored
+                    pythonConnectionInputStream.read(messageLengthBytes);
+
+                    byte[] messageBytes = new byte[Integer.parseInt(new String(messageLengthBytes, StandardCharsets.UTF_8))];
+                    //noinspection ResultOfMethodCallIgnored
+                    pythonConnectionInputStream.read(messageBytes);
+
+                    String message = new String(messageBytes, StandardCharsets.UTF_8);
+                    System.out.print("[" + type + "]  " + message);
+                }
+
+            } catch (Exception e) {
+                System.out.println("Got exception while handling logger");
+                System.out.print(Colors.RED);
+                e.printStackTrace(System.out);
+                System.out.print(Colors.RESET);
+            }
+        }
     }
 
     public void start() {
@@ -46,16 +92,30 @@ public class BetterLoggerCommunicator {
                 openServer();
                 System.out.println("Logging on port " + serverSocket.getLocalPort());
                 connectPython(serverSocket.getLocalPort());
+
+                loggerHandlerThread = new Thread(this::handleLogger);
+                loggerHandlerThread.start();
+
+                started = true;
             } catch (Exception e) {
                 System.out.println(Colors.format("[ERROR] Failed to start logger, error:"));
                 e.printStackTrace();
             }
-            started = true;
+        }
+
+        try
+        {
+            Thread.sleep(10000);
+        }
+        catch(InterruptedException ex)
+        {
+            Thread.currentThread().interrupt();
         }
     }
 
     public void end() {
         try {
+            loggerHandlerThread.join();
             closeServer();
         } catch (Exception e) {
             System.out.println(Colors.format("[ERROR] Failed to end logger"));
--- src/main/resources/com/greenjon902/betterLogger/betterLoggerPortal.py
+++ src/main/resources/com/greenjon902/betterLogger/betterLoggerPortal.py
@@ -10,9 +10,26 @@ conn = socket.create_connection(ip)
 
 
 def send(type_, string):
-    encoded_string = (type_ + ":" + string).encode("utf8")
-    conn.send(len(encoded_string) + encoded_string)
+    encoded_type = f"{len(type_):04}{type_}".encode("utf8")
+    encoded_string = f"{len(string):08}{string}".encode("utf8")
 
+    conn.send(encoded_type + encoded_string)
+
+
+class ConnSender:
+    current_send_type = ""
+
+    def write(self, string):
+        send(self.current_send_type, string)
+
+
+connSender = ConnSender()
+connSender.current_send_type = "LOG"
+sys.stdout = connSender
+
+errorConnSender = ConnSender()
+errorConnSender.current_send_type = "ERROR"
+sys.stderr = errorConnSender
 
 try:
     import betterLogger
@@ -20,12 +37,17 @@ except ImportError:
     send("INFO", "betterLogger not installed")
     send("INFO", "Installing betterLogger")
 
+    connSender.current_send_type = "PIP_INSTALL"
     import subprocess
-    subprocess.call([sys.executable, '-m', 'pip', 'install', "betterLogger"])
+    process = subprocess.run([sys.executable, '-m', 'pip', 'install', "betterLogger"])
+    print(process)
 
+    connSender.current_send_type = "LOG"
     try:
         import betterLogger
     except ImportError:
         send("ERROR", "Install of betterLogger failed")
         sys.exit(1)
     send("INFO", "Installed betterLogger")
+send("CTRL", "End")
+conn.close()
