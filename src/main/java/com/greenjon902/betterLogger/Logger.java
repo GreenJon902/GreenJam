37a55d57c63fbee0e85a6e726fe2644db5e952c5
GreenJon902 <GreenJon902@gmail.com>
2021-12-29 16:10:55 +0000

    added aliases

--- src/main/java/com/greenjon902/betterLogger/Logger.java
+++ src/main/java/com/greenjon902/betterLogger/Logger.java
@@ -54,4 +54,17 @@ public class Logger {
     public void log_critical(String... messages) {
         BetterLogger.log(this, LogLevel.CRITICAL, String.join(" ", messages));
     }
+
+
+    // ALIASES (because shorted and nicer but less explicit depending on the name the variable the loggers stored in) --
+    public void set_name(String name) {set_logger_name(name);}
+    public void push_name(String name) {push_logger_name(name);}
+    public void pop_name(String name) {pop_logger_name(name);}
+    public void dump(String... messages) {log_dump(name);}
+    public void trace(String... messages) {log_trace(name);}
+    public void debug(String... messages) {log_debug(name);}
+    public void info(String... messages) {log_info(name);}
+    public void warning(String... messages) {log_warning(name);}
+    public void error(String... messages) {log_error(name);}
+    public void critical(String... messages) {log_critical(name);}
 }
