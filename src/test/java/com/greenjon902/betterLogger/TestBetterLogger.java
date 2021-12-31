a00bd3911075cf1fdc94a53821f6d4f91a210e5e
GreenJon902 <GreenJon902@gmail.com>
2021-12-31 12:27:50 +0000

    updated test

--- src/test/java/com/greenjon902/betterLogger/TestBetterLogger.java
+++ src/test/java/com/greenjon902/betterLogger/TestBetterLogger.java
@@ -2,6 +2,9 @@ package com.greenjon902.betterLogger;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Field;
+import java.util.Map;
+
 public class TestBetterLogger {
     private void setupLogger() {
         BetterLogger.setup("BetterLoggerTester", "GreenJon902", "V1.69.urMom", "BLT");
@@ -10,12 +13,22 @@ public class TestBetterLogger {
         Runtime.getRuntime().addShutdownHook(new Thread(BetterLogger::finish, "Shutdown-thread"));
     }
 
+    @SuppressWarnings({ "unchecked" })
+    public void updateEnv(String name, String val) throws NoSuchFieldException, IllegalAccessException {
+        Map<String, String> env = System.getenv();
+        Field field = env.getClass().getDeclaredField("m");
+        field.setAccessible(true);
+        ((Map<String, String>) field.get(env)).put(name, val);
+    }
+
+
     @Test
-    public void testLogAtDifferentLevels() {
+    public void testLogAtDifferentLevels() throws NoSuchFieldException, IllegalAccessException {
+        updateEnv("LOG_LEVEL", "1");
         setupLogger();
 
         Logger logger = BetterLogger.getLogger("TestLogger");
-        logger.log_trace("This is trace text and cannot be seen");
+        logger.log_trace("This is trace text");
         logger.log_dump("This is dump text");
         logger.log_debug("This is debug text");
         logger.log_info("This is info text");
