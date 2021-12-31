cc5f0cd04a8eb1642e5816ef3ee51351dda6c7cc
GreenJon902 <GreenJon902@gmail.com>
2021-12-31 12:46:21 +0000

    added parse args function

--- src/main/java/com/greenjon902/greenJam/GreenJam.java
+++ src/main/java/com/greenjon902/greenJam/GreenJam.java
@@ -3,21 +3,33 @@ package com.greenjon902.greenJam;
 import com.greenjon902.betterLogger.BetterLogger;
 import com.greenjon902.betterLogger.Logger;
 
+import java.util.Arrays;
+
 public class GreenJam {
     public static final String NAME = "GreenJam";
     public static final String AUTHOR = "GreenJon902";
     public static final String VERSION = "V0.1-ALPHA";
     public static final String SHORTNAME = "JAM";
 
+    private static Logger logger;
+
     public static void main(String[] args) {
         BetterLogger.setup(NAME, AUTHOR, VERSION, SHORTNAME);
         BetterLogger.start();
 
-        Logger logger = BetterLogger.getLogger(NAME);
+        logger = BetterLogger.getLogger(NAME);
 
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             logger.info("Finishing");
             BetterLogger.finish();
         }, "Shutdown-thread"));
+
+        parseArgs(args);
+    }
+
+    private static void parseArgs(String[] args) {
+        logger.push_logger_name("parseArgs");
+        logger.log_info("Parsing args");
+        logger.debug("Args are", Arrays.toString(args));
     }
 }
