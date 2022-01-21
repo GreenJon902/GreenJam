ac1e2790f7d24998396f301f748ab51bd67080e1
GreenJon902 <GreenJon902@gmail.com>
2022-01-21 17:09:32 +0000

    added TokenClassifiers

--- src/main/java/com/greenjon902/greenJam/config/Config.java
+++ src/main/java/com/greenjon902/greenJam/config/Config.java
@@ -2,8 +2,10 @@ package com.greenjon902.greenJam.config;
 
 public class Config {
     public final LexerTemplates lexerTemplates;
+    public final TokenClassificaters tokenClassificaters;
 
     public Config() {
         lexerTemplates = new LexerTemplates();
+        tokenClassificaters = new TokenClassificaters();
     }
 }
---
+++ src/main/java/com/greenjon902/greenJam/config/TokenClassifiers.java
@@ -0,0 +1,19 @@
+package com.greenjon902.greenJam.config;
+
+public class TokenClassifiers {
+    public final String[] integerClassifiers = {
+            "<[type=integer]>"
+    };
+
+    public final String[]  floatClassifiers = {
+            "<[type=float]>"
+    };
+
+    public final String[] characterClassifiers = {
+            "<[type=character]>"
+    };
+
+    public final String[] stringClassifiers = {
+            "<[type=string]>"
+    };
+}
