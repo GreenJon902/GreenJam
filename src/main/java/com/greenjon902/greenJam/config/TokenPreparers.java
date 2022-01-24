2568bb18d23d6a4194ae2f8f3bef24a15d06e05b
GreenJon902 <GreenJon902@gmail.com>
2022-01-24 18:23:10 +0000

    added first command list for string handling language and first code in that language for token prepareres

--- src/main/java/com/greenjon902/greenJam/config/TokenPreparers.java
+++ src/main/java/com/greenjon902/greenJam/config/TokenPreparers.java
@@ -8,9 +8,44 @@ package com.greenjon902.greenJam.config;
  * CharacterToken(char character)<br/>
  * StringToken(String string)<br/>
  * OperatorToken(OperatorType operatorType, String operator)<br/>
+ *
+ * <table>
+ *   <tr>
+ *       <th>Command</th>
+ *       <th>Use</th>
+ *   </tr>
+ *   <tr>
+ *       <td>ldt</td>
+ *       <td>Load the token type to the accumulator</td>
+ *   </tr>
+ *   <tr>
+ *       <td>ldv</td>
+ *       <td>Load the token value to the accumulator</td>
+ *   </tr>
+ *   <tr>
+ *       <td>sse (string)</td>
+ *       <td>If the value in the accumulator is equal to the value in (string) then skip to the next preparer, else carry on</td>
+ *   </tr>
+ *   <tr>
+ *       <td>cse (string)</td>
+ *       <td>If the value in the accumulator is equal to the value in (string) then continue, else skip to the next preparer</td>
+ *   </tr>
+ *   <tr>
+ *       <td>sac</td>
+ *       <td>Set the current preparer as correct and will exit when finishes the preparer</td>
+ *   </tr>
+ *   <tr>
+ *       <td>sta (arg_number)</td>
+ *       <td>Set the argument at (arg_number)'s value to the current value in the accumulator. If the arg number is higher then the amount of args that the function takes then it is ignored</td>
+ *   </tr>
+ * </table>
  */
 public class TokenPreparers {
-    public final String[][] tokenPreparers = {
-    };
+    public final String integerPreparer =
+            "ldt;" +
+            "cse \"integer\";" +
+            "sac;" +
+            "ldv;" +
+            "set 0";
 
 }
