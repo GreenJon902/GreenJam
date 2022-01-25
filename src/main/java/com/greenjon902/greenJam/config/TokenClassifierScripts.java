a3c6fc4ae33f82b60e3d3794b43d4dcfdefeb4ec
GreenJon902 <GreenJon902@gmail.com>
2022-01-25 16:26:47 +0000

    UPDATE INSTRUCTIONS 1: updated TokenClassifierScripts' docstring to have the new commands

--- src/main/java/com/greenjon902/greenJam/config/TokenClassifierScripts.java
+++ src/main/java/com/greenjon902/greenJam/config/TokenClassifierScripts.java
@@ -23,12 +23,12 @@ package com.greenjon902.greenJam.config;
  *       <td>Load the token value to the accumulator</td>
  *   </tr>
  *   <tr>
- *       <td>sse (string)</td>
- *       <td>If the value in the accumulator is equal to the value in (string) then skip to the next preparer, else carry on</td>
+ *       <td>equ (string)</td>
+ *       <td>If the value in the accumulator is equal to the value in (string) then set the accumulator to "TRUE", if it is not then set the accumulator to "FALSE"</td>
  *   </tr>
  *   <tr>
- *       <td>cse (string)</td>
- *       <td>If the value in the accumulator is equal to the value in (string) then continue, else skip to the next preparer</td>
+ *       <td>flp</td>
+ *       <td>If the value in the accumulator contains "TRUE" then set it to "FALSE" and vice-versa, If it does not equal either of them then an error is thrown</td>
  *   </tr>
  *   <tr>
  *       <td>stt (string)</td>
@@ -36,8 +36,52 @@ package com.greenjon902.greenJam.config;
  *   </tr>
  *   <tr>
  *       <td>sta (arg_number)</td>
- *       <td>Set the argument at (arg_number)'s value to the current value in the accumulator. If the arg number is higher then the amount of args that the function takes then it is ignored</td>
+ *       <td>Set the argument at (arg_number)'s value to the current value in the accumulator. If the arg number is higher then the amount of args that the function takes then it is ignored. This can also be used as memory</td>
  *   </tr>
+ *   <tr>
+ *       <td>lta (arg_number)</td>
+ *       <td>Load the argument at (arg_number)'s value to the accumulator, crashes if not set</td>
+ *   </tr>
+ *   <tr>
+ *       <td>err (error_message)</td>
+ *       <td>Throw an error with the message inside of (error_message). This can include %acc to format in the accumulator value or %arg(arg_number) to format a token argument value from the location (arg_number) in</td>
+ *  </tr>
+ *  <tr>
+ *      <td>sit</td>
+ *      <td>Skip to the next preparer script if accumulator contains "TRUE"</td>
+ *  </tr>
+ *  <tr>
+ *      <td>sif</td>
+ *      <td>Skip to the next preparer script if accumulator contains "FALSE"</td>
+ *  </tr>
+ *  <tr>
+ *      <td>skp</td>
+ *      <td>Skip to the next preparer script</td>
+ *  </tr>
+ *  <tr>
+ *      <td>jit (location)</td>
+ *      <td>Jump to the instruction at (location) if accumulator contains "TRUE"</td>
+ *  </tr>
+ *  <tr>
+ *      <td>jif (location)</td>
+ *      <td>Jump to the instruction at (location) if accumulator contains "FALSE"</td>
+ *  </tr>
+ *  <tr>
+ *      <td>jmp (location)</td>
+ *      <td>Jump to the instruction at (location)</td>
+ *  </tr>
+ *  <tr>
+ *      <td>mit (location)</td>
+ *      <td>Move to the instruction that is the current location + (location) if accumulator contains "TRUE"</td>
+ *  </tr>
+ *  <tr>
+ *      <td>mif (location)</td>
+ *      <td>Move to the instruction that is the current location + (location) if accumulator contains "FALSE"</td>
+ *  </tr>
+ *  <tr>
+ *      <td>mov (location)</td>
+ *      <td>Move to the instruction that is the current location + (location)</td>
+ *  </tr>
  * </table>
  */
 public class TokenClassifierScripts {
