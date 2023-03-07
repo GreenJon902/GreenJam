dde9906d73e6dd6a3c632843f3cac17907a130d4
GreenJon902 <greenjon902@gmail.com>
2023-03-07 21:00:43 +0000

    split up standard jam tests

--- src/test/java/com/greenjon902/greenJam/parser/ParserTest.java
+++ src/test/java/com/greenjon902/greenJam/parser/ParserTest.java
@@ -6,9 +6,6 @@ import com.greenjon902.greenJam.common.SyntaxContext;
 import com.greenjon902.greenJam.instructionHandler.StandardInstructionHandler;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
-
-import static com.greenjon902.greenJam.parser.ParserTestResources.getPack;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ParserTest {
@@ -60,62 +57,5 @@ public class ParserTest {
 		System.out.println(node.format());
 
 		assertTrue(string.isEnd());
-	}
-
-	@Test
-	void testWithStandardJam() throws IOException {
-		SyntaxContext syntaxContext = new SyntaxContext();
-		StandardInstructionHandler instructionHandler = new StandardInstructionHandler(syntaxContext);
-		Parser parser = new Parser(instructionHandler, syntaxContext);
-
-		StringInputStream pack = StringInputStream.from(getPack("jam.jam"));
-		parser.parse(pack);
-		printParseResult(parser, "integer_literal", "12");
-		printParseResult(parser, "integer_literal", "+12");
-		printParseResult(parser, "integer_literal", "-12");
-		printParseResult(parser, "identifier", "tes_t43");
-		printParseResult(parser, "identifier", "f");
-		printParseResult(parser, "identifier", "0hey");
-		printParseResult(parser, "variable_declaration", "public String test");
-		printParseResult(parser, "variable_declaration", "private static String test");
-		printParseResult(parser, "variable_declaration", "String test");
-		printParseResult(parser, "variable_setting", "static String test = 5");
-		printParseResult(parser, "variable_setting", "test = another_test_var");
-		printParseResult(parser, "expression", "foo * bar + 5");
-		printParseResult(parser, "expression", "foo->bar * (6 + 3)");
-		printParseResult(parser, "variable_setting", "test = test->bar * 6");
-		printParseResult(parser, "line", "test = test->bar * 6;");
-		printParseResult(parser, "code_block", "{test = test->bar * 6;}");
-
-		printParseResult(parser, "full_function_arguments", "A b");
-		printParseResult(parser, "full_function_arguments", "A b, C d");
-		printParseResult(parser, "full_function_call_arguments", "a, b");
-		printParseResult(parser, "function_call", "foo(bar, baz)");
-		printParseResult(parser, "line", "bar = foo->test(bar, baz);");
-		printParseResult(parser, "function_declaration",
-				"""
-						public String foo(Bar a, Baz b) {
-							int result = a->calculator->calculate(b);
-							return String(result);
-						}
-						""");
-	}
-
-	void printParseResult(Parser parser, String root_node, String string) {
-		StringInputStream stringStream = new StringInputStream("<test1>",
-				";; ROOT_NODE SET " + root_node + ";" +
-						string);
-		AstNode result = parser.parse(stringStream);
-
-		System.out.println(root_node);
-		System.out.println(string);
-		System.out.println(result);
-		if (result == null) {
-			System.out.println();
-		} else {
-			System.out.println(result.format());
-		}
-		System.out.println(stringStream.location + "/" + stringStream.string.length());
-		System.out.println();
 	}
 }
